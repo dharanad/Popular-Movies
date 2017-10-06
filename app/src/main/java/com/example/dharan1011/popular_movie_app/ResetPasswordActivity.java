274b4899c78dca3b541a0c3ade2ed5021f84f997
ABHISHEK RAJ <rajtheinnovatorabhishek@gmail.com>
2017-10-06 20:13:25 +0530

    Added LoginActivity

---
+++ app/src/main/java/com/example/dharan1011/popular_movie_app/ResetPasswordActivity.java
@@ -0,0 +1,13 @@
+package com.example.dharan1011.popular_movie_app;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ResetPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
